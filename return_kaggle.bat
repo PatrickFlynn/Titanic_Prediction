f8e94fe85faf62a4b680f3ce32f7bc77fd531a38
Patrick Flynn <patrickseanflynn@gmail.com>
2020-01-03 14:01:28 -0800

    [wip] Use random forest to get 75% kaggle score

---
+++ return_kaggle.bat
@@ -0,0 +1,1 @@
+S: && cd "S:\Code\Anaconda\Scripts" && activate DataScience && cd "S:\Code\Data Science\Titanic_Prediction" && kaggle competitions submit -c titanic -f submission.csv -m "Random Forest" && kaggle competitions submissions -c titanic > Kaggle_Submits.txt
