d4257062365927d7927184c7a806ad5296c24fde
Patrick Flynn <patrickseanflynn@gmail.com>
2020-01-06 19:08:48 -0800

    [wip] Add ticket based functionality - 77% Kaggle

--- return_kaggle.bat
+++ return_kaggle.bat
@@ -1,1 +1,1 @@
-S: && cd "S:\Code\Anaconda\Scripts" && activate DataScience && cd "S:\Code\Data Science\Titanic_Prediction" && kaggle competitions submit -c titanic -f submission.csv -m "Random Forest" && kaggle competitions submissions -c titanic > Kaggle_Submits.txt
+S: && cd "S:\Code\Anaconda\Scripts" && activate DataScience && cd "S:\Code\Data Science\Titanic_Prediction" && kaggle competitions submit -c titanic -f submission.csv -m "Random Forest" && TIMEOUT 120 && kaggle competitions submissions -c titanic > Kaggle_Submits.txt
