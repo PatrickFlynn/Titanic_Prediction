b73c80be939c773834862d2c5046e4d7ebc3f9e6
Patrick Flynn <patrickseanflynn@gmail.com>
2020-01-14 18:47:28 -0800

    [wip] Test ensemble voting further

--- return_kaggle.bat
+++ return_kaggle.bat
@@ -1,1 +1,3 @@
-S: && cd "S:\Code\Anaconda\Scripts" && activate DataScience && cd "S:\Code\Data Science\Titanic_Prediction" && kaggle competitions submit -c titanic -f submission.csv -m "Random Forest" && TIMEOUT 120 && kaggle competitions submissions -c titanic > Kaggle_Submits.txt
+@echo off
+set /p id="Comment: "
+S: && cd "S:\Code\Anaconda\Scripts" && activate DataScience && cd "S:\Code\Data Science\Titanic_Prediction" && kaggle competitions submit -c titanic -f submission.csv -m "%id%" && TIMEOUT 120 && kaggle competitions submissions -c titanic > Kaggle_Submits.txt
